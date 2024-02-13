0300b8f23b9e97dac0e80cf1af6c320a16fef1f3
Winstone-Were <stoniedev@gmail.com>
2024-02-13 07:56:47 +0300

    get album by ID

--- main.go
+++ main.go
@@ -34,8 +34,22 @@ func postAlbum(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, newAlbum);
 }
 
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 func main(){
 	router := gin.Default()
+	router.GET("/albums/:id",getAlbumByID)
 	router.GET("/albums", getAlbums);
 	router.POST("/albums", postAlbum);
 
